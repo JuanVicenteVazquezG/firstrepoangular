e82bfd3d867c1d9711ebdde5c8c7aeb62341076d
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2020-08-25 18:50:47 +0200

    Created component proofs
    Create component proofs with "ng g component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyComponent } from '../components/mycomponent/mycomponent.component';
+import { MoviesComponent } from '../components/movies/movies.component';
+import { ProofsComponent } from '../components/proofs/proofs.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    MyComponent
+    AppComponent,
+    MyComponent,
+    MoviesComponent,
+    ProofsComponent,
   ],
   imports: [
     BrowserModule
